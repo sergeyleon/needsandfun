76f1d629294a5c8a680213decda6c7d7369bda41
Vasily <bazzy@yandex.ru>
2012-08-13 15:56:40 +0400

    Update app/bundles/needsandfun/controller/market.php

--- app/bundles/needsandfun/controller/market.php
+++ app/bundles/needsandfun/controller/market.php
@@ -7,7 +7,7 @@ class Market extends \Core\Abstracts\Singleton
 	public function clickAndPay()
 	{
 		 $options = array(
-   'conditions' => '(supplier_id=3 or supplier_id=13 or supplier_id=15 or supplier_id=18) or in_stock>0 and is_available=1 and deleted is null
+   'conditions' => '((supplier_id=3 or supplier_id=13 or supplier_id=15 or supplier_id=18) or in_stock>0) and is_available=1 and deleted is null
 				and id in (
 					select 
 						good_id 
