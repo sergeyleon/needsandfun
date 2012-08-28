230626fe84c4bc41e79aa6378093836ce2c2cb89
unknown <Sergey@.(none)>
2012-08-28 15:26:46 +0300

    fix manage order

--- app/bundles/managers/controller/orders.php
+++ app/bundles/managers/controller/orders.php
@@ -69,6 +69,10 @@ class Orders extends \Core\Abstracts\Singleton
     private function _form()
     {
         $this->page['statuses'] = \Core\Model\Status::getAll();
+        
+        $this->page['types']      = \Core\Model\Deliverytype::getAll();
+        $this->page['metrolines'] = \Core\Model\Metroline::getAll();
+        
         $this->page->display('orders/form.twig');
     }
     
@@ -78,12 +82,37 @@ class Orders extends \Core\Abstracts\Singleton
             ? new \Core\Model\Order()
             : \Core\Model\Order::find($values['id']);
         
+        $delivery = \Core\Model\Delivery::all(array('conditions' => array('order_id = ? ', $item->id )));
+        
+        $delivery = $delivery[0];
+        
+        if($values['delivery_price']) { $values['delivery_price'] = null; }
+        $delivery->delivery_price = $values['delivery_price'];
+        
+        $delivery->metro_id = !empty($values['metro'])
+            ? $values['metro']
+            : null;
+        
+        if (!empty($values['delivery_date']))
+        {
+            $delivery->delivery_date    = new \DateTime($values['delivery_date']);
+        }
+        
+        $delivery->comment          = $values['comment'];
+        $delivery->address          = $values['address'];
+        $delivery->recall           = $values['recall'];
+        $delivery->delivery_type_id = $values['type'];
+        
+        $delivery -> save();
+
+        
         if ($values['status'] != $item->getStatus()->id)
         {
             $item->setStatus($values['status']);
         }
         
         $item -> save();
+        
 
         $this->router->go($this->router->generate('manage_orders_index'));
     }
